c18631491a1595e72a8a0ee3e30da5d9e387912c
Yiwei Chiao <ywchiao@gmail.com>
2017-12-15 13:09:28 +0800

    gridline added.

--- htdocs/js/sokoban.js
+++ htdocs/js/sokoban.js
@@ -31,6 +31,38 @@ window.addEventListener('load', () => {
   gameHeader.appendChild(gameTitle);
 
   let sokobanCanvas = document.createElement('canvas');
+  let ctxPaint = sokobanCanvas.getContext('2d');
+
+  // 設定繪圖圖紙的寬高
+  sokobanCanvas.width = 256;
+  sokobanCanvas.height = 256;
+
+  // 將圖紙埴滿背景色
+  ctxPaint.fillStyle = 'mintcream';
+  ctxPaint.fillRect(0, 0, sokobanCanvas.width, sokobanCanvas.height);
+
+  // 準備一支可以畫 _斷續線_ 的畫筆
+  ctxPaint.strokeStyle = 'black';
+  // 斷續線由連續 4px，再空白 4px構成
+  ctxPaint.setLineDash([4, 4]);
+
+  // 開始記録格線的 paths
+  ctxPaint.beginPath();
+
+  // 畫 8 條鉛直斷續線
+  for (var c = 1; c < 8; c ++) {
+    ctxPaint.moveTo(c * 32, 0);
+    ctxPaint.lineTo(c * 32, 320);
+  }
+
+  // 畫 8 條水平斷續線
+  for (var r = 1; r < 8; r ++) {
+    ctxPaint.moveTo( 0, r * 32);
+    ctxPaint.lineTo(640, r * 32);
+  }
+
+  // 繪出格線
+  ctxPaint.stroke();       
 
   let sokobanBoard = document.createElement('div');
   sokobanBoard.style.gridArea = '1 / 2 / 4 / 5';
