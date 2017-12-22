2c649dd332f464fec9348dab07883dca32760823
Yiwei Chiao <ywchiao@gmail.com>
2017-12-22 13:23:07 +0800

    add drawGameBoard.

--- htdocs/js/sokoban.js
+++ htdocs/js/sokoban.js
@@ -3,14 +3,160 @@
  *  @brief      The entry file of Sokoban.
  *  @author     Yiwei Chiao (ywchiao@gmail.com)
  *  @date       11/17/2017 created.
- *  @date       12/07/2017 last modified.
- *  @version    0.1.0
+ *  @date       12/22/2017 last modified.
+ *  @since      0.1.0
  *  @copyright  MIT, (C) 2017 Yiwei Chiao
  *  @details
  *
  *  The entry file of Sokoban.
  */
 'use strict';
+
+/**
+ * sokoban 關卡描述
+ *
+ *  #    牆壁 (wall)
+ *  @    玩家 (player)
+ *  $    箱子 (box)
+ *  .    目標點 (goal)
+ *  +    玩家站在目標點上 (player on goal square)
+ *  *    箱子在目標點上 (box on goal square)
+ *  空白 地板 (floor)
+ */
+let levels = {
+  'level_0': [
+    "############",
+    "#         .#",
+    "#          #",
+    "#          #",
+    "#   ####   #",
+    "#          #",
+    "#          #",
+    "#    $     #",
+    "#    @     #",
+    "#          #",
+    "#          #",
+    "############"
+  ],
+
+  'level_1': [
+     "------------",
+     "------------",
+     "--#######---",
+     "--# ..$ #---",
+     "--# # $ #---",
+     "--# # # #---",
+     "--# $@# #---",
+     "--#.$   #---",
+     "--#.#####---",
+     "--###-------",
+     "------------",
+     "------------"
+   ],
+};
+
+/**
+ * 準備繪圖用的 sprites。
+ *
+ * @returns sprites 集合物件。
+ */
+let sprites = ((spriteSheet) => {
+  let baseX = 0;
+  let baseY = 6 * 64;
+
+  let tileset = new Image();
+  tileset.src = spriteSheet;
+
+  let tile = (tileset, sx, sy, ctx) => {
+    ctx.drawImage(
+      tileset,
+      sx, sy, 32, 32,
+      0, 0, 32, 32
+    );
+  };
+
+  return {
+    box: tile.bind(null, tileset, baseX, baseY),
+    boxOnGoal: tile.bind(null, tileset, baseX + 32, baseY),
+    wall: tile.bind(null, tileset, baseX + 64, baseY),
+
+    floor: tile.bind(null, tileset, baseX, baseY + 32),
+    goal: tile.bind(null, tileset, baseX + 32, baseY + 32),
+    grass: tile.bind(null, tileset, baseX + 64, baseY + 32),
+
+    faceRight: tile.bind(null, tileset, baseX, baseY + 64),
+    faceDown: tile.bind(null, tileset, baseX + 32, baseY + 64),
+
+    faceUp: tile.bind(null, tileset, baseX, baseY + 96),
+    faceLeft: tile.bind(null, tileset, baseX + 32, baseY + 96),
+  };
+})('SokobanClone_byVellidragon.png');
+
+/**
+ * 依遊戲狀態，繪出盤面
+ *
+ * @param 'ctx' : 繪圖 context 物件
+ * @returns {undefined}
+ */
+let drawGameBoard = (ctx, gameState) => {
+  let height = gameState.level.length;
+
+  for (let x = 0; x < height; x ++) {
+    for (let y = 0; y < height; y ++) {
+      ctx.save();
+      ctx.translate(32*x, 32*y);
+
+      switch (gameState.level[y].charAt(x)) {
+        case '#': 
+          sprites.wall(ctx);
+
+          break;
+
+        case '$': 
+          sprites.box(ctx);
+
+          break;
+
+        case '@':
+          sprites.floor(ctx);
+          sprites.faceUp(ctx);
+
+          break;
+
+        case ' ':
+          sprites.floor(ctx);
+
+          break;
+
+        case '.':
+          sprites.goal(ctx);
+
+          break;
+
+        case'*':
+          sprites.boxOnGoal(ctx);
+
+          break;
+
+        case'+':
+          sprites.goal(ctx);
+          sprites.faceUp(ctx);
+
+          break;
+
+        case'-':
+          sprites.grass(ctx);
+
+          break;
+
+        default:
+          console.log('Wrong map data');
+      }
+
+      ctx.restore();
+    };
+  };
+};
 
 /**
  * sokoban 程式進入點
@@ -34,8 +180,8 @@ window.addEventListener('load', () => {
   let ctxPaint = sokobanCanvas.getContext('2d');
 
   // 設定繪圖圖紙的寬高
-  sokobanCanvas.width = 256;
-  sokobanCanvas.height = 256;
+  sokobanCanvas.width = 32*12
+  sokobanCanvas.height = 32*12;
 
   // 將圖紙埴滿背景色
   ctxPaint.fillStyle = 'mintcream';
@@ -49,14 +195,14 @@ window.addEventListener('load', () => {
   // 開始記録格線的 paths
   ctxPaint.beginPath();
 
-  // 畫 8 條鉛直斷續線
-  for (var c = 1; c < 8; c ++) {
+  // 畫 12 條鉛直斷續線
+  for (var c = 1; c < 12; c ++) {
     ctxPaint.moveTo(c * 32, 0);
-    ctxPaint.lineTo(c * 32, 320);
+    ctxPaint.lineTo(c * 32, 32*12);
   }
 
-  // 畫 8 條水平斷續線
-  for (var r = 1; r < 8; r ++) {
+  // 畫 12 條水平斷續線
+  for (var r = 1; r < 12; r ++) {
     ctxPaint.moveTo( 0, r * 32);
     ctxPaint.lineTo(640, r * 32);
   }
@@ -95,6 +241,8 @@ window.addEventListener('load', () => {
     document.getElementById('cursor_x').textContent = e.clientX;
     document.getElementById('cursor_y').textContent = e.clientY;
   });
+
+  drawGameBoard(ctxPaint, { level: levels.level_0 });
 });
 
 // index.js
